48140f58c58eed860bef3ba0039334486445e5fa
peter <peter.k.nets@gmail.com>
2016-02-29 20:31:35 +0200

    Unbind scrolling after all animations are over

--- js/app.js
+++ js/app.js
@@ -9,16 +9,17 @@ $(document).ready(function() {
       dtCollection = $('.book-dt-1,.book-dt-2,.book-dt-3,.book-dt-4,.book-dt-5,.book-dt-6,.book-dt-7'),
       ssCollection = $('.book-ss-1,.book-ss-2,.book-ss-3,.book-ss-4,.book-ss-5,.book-ss-6,.book-ss-7,.book-ss-8,.book-ss-9,.book-ss-10'),
       dt1_flag = true,
-      ss1_flag = true;
+      ss1_flag = true,
+      playedAnimations = [];
 
-  // Function for animating picture collections
+  // Function to animate picture collections
   var animate = function(collection) {
     collection.each(function(index) {
       $(this).delay(index*(duration)).animate({'opacity': 1, 'left': '+=20'}, duration);
     });
   };
 
-  // Function for changing picture collections position on window.resize
+  // Function to change picture collections position on window.resize
   var changePosition = function(collection, countValue) {
     var count = 30;
     collection.each(function() {
@@ -27,37 +28,56 @@ $(document).ready(function() {
     });
   };
 
-  /* Every time the window is scrolled */
-  $(window).scroll(function() {
+  // Function to check animations status
+  var checkAnimations = function(animations) {
+    animations.each(function() {
+      if (playedAnimations.indexOf($(this)) < 0) {
+        return true;
+      }
+    });
+    return false;
+  };
 
-    if ($(window).width() > 970) {
-      /* Check location of each animated element */
-      $animatedObjects.each(function() {
-        var bottomOfObject = $(this).offset().top + $(this).outerHeight();
-        var bottomOfWindow = $(window).scrollTop() + $(window).height();
-        /* If object is completely visible, fade it in */
-        if (bottomOfWindow > bottomOfObject) {
-          $(this).animate({'opacity': 1, 'left': 0}, 500);
+    /* Every time the window is scrolled */
+    $(window).scroll(function() {
+      /* Check if all animations already played */
+      if (checkAnimations($animatedObjects) || dt1_flag || ss1_flag) {
+        console.log("scrolling...");
+
+        if ($(window).width() > 970) {
+          /* Check location of each animated element */
+          $animatedObjects.each(function () {
+            var bottomOfObject = $(this).offset().top + $(this).outerHeight();
+            var bottomOfWindow = $(window).scrollTop() + $(window).height();
+            /* If object is completely visible, fade it in */
+            if (bottomOfWindow > bottomOfObject) {
+              $(this).animate({'opacity': 1, 'left': 0}, 500);
+              playedAnimations.push($(this));
+            }
+          });
+          /* Animation for Dark Tower and Short Story Books */
+          var dt1_bottom = $(dtCollection[0]).offset().top + $(dtCollection[0]).outerHeight(),
+            ss1_bottom = $(ssCollection[0]).offset().top + $(ssCollection[0]).outerHeight(),
+            bottomOfWindow = $(window).scrollTop() + $(window).height();
+          if (dt1_flag) {
+            if (bottomOfWindow > dt1_bottom) {
+              animate(dtCollection);
+              dt1_flag = false;
+            }
+          }
+          if (ss1_flag) {
+            if (bottomOfWindow > ss1_bottom) {
+              animate(ssCollection);
+              ss1_flag = false;
+            }
+          }
         }
-      });
-      /* Animation  for Dark Tower Books */
-      var dt1_bottom = $(dtCollection[0]).offset().top + $(dtCollection[0]).outerHeight(),
-        ss1_bottom = $(ssCollection[0]).offset().top + $(ssCollection[0]).outerHeight(),
-        bottomOfWindow = $(window).scrollTop() + $(window).height();
-      if (dt1_flag) {
-        if (bottomOfWindow > dt1_bottom) {
-          animate(dtCollection);
-          dt1_flag = false;
-        }
+      } else {
+        console.log("All over!");
+        $(window).unbind('scroll');
       }
-      if (ss1_flag) {
-        if (bottomOfWindow > ss1_bottom) {
-          animate(ssCollection);
-          ss1_flag = false;
-        }
-      }
-    }
-  });
+    });
+
 
   /* Every time the window is resized */
   $(window).resize(function() {
